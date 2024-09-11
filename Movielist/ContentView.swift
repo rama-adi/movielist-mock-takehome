37d583ff1f02d3eae38e90902b32aeaa2f137b0c
ramaadi <17980909+rama-adi@users.noreply.github.com>
2024-09-11 14:44:45 +0700

    Initial Commit

---
+++ Movielist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movielist
+//
+//  Created by Rama Adi Nugraha on 11/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movielist/MovielistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovielistApp.swift
+//  Movielist
+//
+//  Created by Rama Adi Nugraha on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MovielistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
